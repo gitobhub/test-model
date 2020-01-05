7c4f616253ed79b5cc18174ce93817cb08bba499
gitobhub <lgob_123@163.com>
2020-01-05 13:09:03 +0800

    Update example

--- example/main.cpp
+++ example/main.cpp
@@ -1,16 +1,24 @@
 #include "TestModel.h"
 
-class TestHelloworld : TestModel<> {
+class TestHelloworld : public TestModel<Nil> {
 public:
-    TestHelloworld() : TestModel(this) {
+    TestHelloworld() : TestModel(this) {}
+    
+    void Work() override {
         Item item("hello world");
         ItemWrapper(item).Print();
 
         int one = Get<int>("one"); //{"one": 1}
 
-        vector<int> ivec = Array<int>("uids"); //{"uids" : [1, 2, 3]}
+        ItemWrapper(
+            Item(one),
+            Item(Get<int>("two")),
+            Item(Get<int>("three", "value"))
+        ).Print();
 
-        Get<string>("family", "father", "name");    //{"family": {"father": {"name": "bb"}}}
+//        vector<int> ivec = Array<int>("uids"); //{"uids" : [1, 2, 3]}
+
+//        Get<string>("family", "father", "name");    //{"family": {"father": {"name": "bb"}}}
 
         ItemWrapper(
             Item(1, "%d: Hello world"),
@@ -19,17 +27,17 @@ public:
     }
 };
 
-class TestSubExample : TestModel<TestHelloworld> {
+class TestSubExample : public TestModel<TestHelloworld> {
 public:
     TestSubExample() : TestModel(this) {}
 };
 
-class TestExample : TestMoel<TestSubExample> {
+class TestExample : public TestModel<TestSubExample> {
 public:
     TestExample() : TestModel(this) {}
 };
 
 int main()
 {
-    TestExample();
+    TestExample().Run();
 }
